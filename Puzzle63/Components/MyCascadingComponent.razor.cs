c49a1f9bdd94a585531575c47322f810d202b203
Carl Franklin <carl@franklins.net>
2025-01-21 07:49:51 -0500

    Add project files.

---
+++ Puzzle63/Components/MyCascadingComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Puzzle63.Components;
+
+public partial class MyCascadingComponent : ComponentBase
+{
+	[Parameter]
+	public RenderFragment ChildContent { get; set; }
+
+	[Parameter]
+	public EventCallback<string> MyEventCallback { get; set; }
+
+	private Timer Timer { get; set; }
+
+	protected override void OnAfterRender(bool firstRender)
+	{
+		if (firstRender)
+		{
+			Timer = new Timer(OnTimerElapsed, null, 1000, 1000);
+		}
+	}
+
+	private void OnTimerElapsed(object state)
+	{
+		InvokeAsync(() => MyEventCallback.InvokeAsync(DateTime.Now.ToString("T")));
+	}
+
+}
